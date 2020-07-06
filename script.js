dd1e229fc135fae1d8c49dc13230acd9c9442253
Dan <daniel.murphy.1@gmail.com>
2020-07-06 01:14:25 -0400

    added functionality for list to work, not with local storage

--- script.js
+++ script.js
@@ -2,42 +2,89 @@ const addBtn = document.getElementById("addBtn");
 const addItem = document.getElementById("addItem");
 const ul = document.getElementById("list");
 const error = document.getElementById("error");
+//let toDoList = [];
 
 
 
 const addItemToList = function(){
     if (addItem.value === ""){
         event.preventDefault();
-        error.classList.remove("error");
+        error.classList.remove("hidden");
     }else {
-        error.classList.add("error");
+        error.classList.add("hidden");
         console.log(addItem.value);
         event.preventDefault();
         let li = document.createElement("li");
-        let randomID = Math.random();
-        console.log(randomID);
-        li.setAttribute("id", randomID);
+        let listItemID = Math.random();
+        let checkboxID = Math.random();
+        let deleteID = Math.random();
+        console.log(listItemID);
+        li.setAttribute("id", listItemID);
         let checkbox = document.createElement("input");
             checkbox.type = "checkbox";
-            checkbox.id =  randomID;
+            checkbox.id =  checkboxID;
             checkbox.name = addItem.value;
-            checkbox.value = randomID;
-        //li.appendChild(checkbox);
+            checkbox.value = checkboxID;
+        
         let deleteButton = document.createElement("button");
-            deleteButton.setAttribute("id", randomID);
+            deleteButton.setAttribute("id", deleteID);
             deleteButton.setAttribute("class", "delete-btn");
+            deleteButton.classList.add("hidden");
             deleteButton.textContent = "Delete";
-        li.textContent = addItem.value; 
+      
+       let liText = document.createTextNode(addItem.value);
         
         li.appendChild(checkbox);
+        li.appendChild(liText);
         li.appendChild(deleteButton);
         ul.appendChild(li);
         addItem.value = "";
-        const deleteBtn = document.getElementById(randomID); 
+
+        //toDoList.push(li);
+          
+
+
+         
+         //window.localStorage.setItem('user2', JSON.stringify(toDoList));
+        
+
+        const deleteBtn = document.getElementById(deleteID); 
         deleteBtn.addEventListener("click", function(){
-            this.remove();
+            li.remove();
+            // const index = toDoList.indexOf(li);
+            // if (index > -1){
+            //     toDoList.splice(index, 1);
+            // }
+            
+            // window.localStorage.setItem('user', JSON.stringify(toDoList));
+           
+
         });
+       
+
+        checkbox.addEventListener("click",function(){
+            deleteButton.classList.toggle("hidden");
+            li.classList.toggle("complete");
+        });
+        
+        
     }
+
+    
 };
 
+// window.onload = function(){
+//     toDoList = JSON.parse(window.localStorage.getItem('user'));
+//     for (let i = 0; i < toDoList.length; i++){
+//         console.log(toDoList[i]);
+//         let li = document.createElement("li");
+//         li.innerHTML = toDoList[i];
+//         ul.appendChild(li);
+//     }
+// }
+
+
 addBtn.addEventListener("click", addItemToList);
+
+
+
