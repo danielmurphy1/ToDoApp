c876f37f4b06aa4ff15353f6925ba9b7b19f447d
Dan <daniel.murphy.1@gmail.com>
2020-07-08 00:09:34 -0400

    added style, cleaned up code

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const addBtn = document.getElementById("addBtn");
 const addItem = document.getElementById("addItem");
 const ul = document.getElementById("list");
@@ -5,8 +6,7 @@ const error = document.getElementById("error");
 let toDoList = [];
 
 
-
-const addItemToList = function(){
+addBtn.addEventListener("click",  function(){
     if (addItem.value === ""){
         event.preventDefault();
         error.classList.remove("hidden");
@@ -16,23 +16,18 @@ const addItemToList = function(){
         event.preventDefault();
         let li = document.createElement("li");
         let listItemID = Math.random();
-        let checkboxID = Math.random();
-        let deleteID = Math.random();
-        console.log(listItemID);
         li.setAttribute("id", listItemID);
+        let liText = document.createTextNode(addItem.value);
+
         let checkbox = document.createElement("input");
             checkbox.type = "checkbox";
-            checkbox.id =  checkboxID;
-            checkbox.name = addItem.value;
-            checkbox.value = checkboxID;
+            
         
         let deleteButton = document.createElement("button");
-            deleteButton.setAttribute("id", deleteID);
-            deleteButton.setAttribute("class", "delete-btn");
             deleteButton.classList.add("hidden");
             deleteButton.textContent = "Delete";
       
-       let liText = document.createTextNode(addItem.value);
+       
         
         li.appendChild(checkbox);
         li.appendChild(liText);
@@ -40,8 +35,6 @@ const addItemToList = function(){
         ul.appendChild(li);
         
         
-        
-        //debugger;
         const toDoItem = {
             id: listItemID, 
             text: addItem.value,
@@ -55,7 +48,7 @@ const addItemToList = function(){
          window.localStorage.setItem('user', JSON.stringify(toDoList));
         
 
-        //const deleteBtn = document.getElementById(deleteID); 
+     
 
         deleteButton.addEventListener("click", function(){
             li.remove();
@@ -73,49 +66,45 @@ const addItemToList = function(){
         checkbox.addEventListener("click",function(){
             deleteButton.classList.toggle("hidden");
             li.classList.toggle("complete");
+            
         });
         
         
     }
 
     
-};
+});
 
 window.onload = function(){
     toDoList = JSON.parse(window.localStorage.getItem('user')) || [];
     for (let i = 0; i < toDoList.length; i++){
         console.log(toDoList[i]);
         
-        //create an li element based on the current todo element looping through
+        
 
         let li = document.createElement("li");
         let listItemID = toDoList[i]["id"];
         li.setAttribute("id", listItemID);
         let liText = document.createTextNode(toDoList[i]["text"]);
+
+
         let checkbox = document.createElement("input");
             checkbox.type = "checkbox";
-            //checkbox.id =  checkboxID;
-            checkbox.name = toDoList[i]["text"];
-            //checkbox.value = checkboxID;
+           
 
         let deleteButton = document.createElement("button");
-            //deleteButton.setAttribute("id", deleteID);
-            deleteButton.setAttribute("class", "delete-btn");
             deleteButton.classList.add("hidden");
             deleteButton.textContent = "Delete";
 
-        //debugger;
+        
         li.appendChild(checkbox);
         li.appendChild(liText);
         li.appendChild(deleteButton);
         ul.appendChild(li);
 
-        //const deleteBtn = document.getElementById(deleteID); 
-       
-        //debugger;
+        
         deleteButton.addEventListener("click", function(){
             li.remove();
-          
             const index = toDoList.findIndex(item => item.id === listItemID);
             if (index > -1){
                 toDoList.splice(index, 1);
@@ -132,9 +121,5 @@ window.onload = function(){
         });
     }
 }
-
-
-addBtn.addEventListener("click", addItemToList);
-
 
 
