63a7d0863341b0c53ee6e1655211aea31714e9d5
Dan <daniel.murphy.1@gmail.com>
2020-11-01 20:42:08 -0500

    refactored repeated code into reuseable functions in script.js, removed global variables so that variables were only created when needed, edited README file.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ _Simple Web Based ToDo App Written in HTML, CSS, and JavaScript_
 
 Add items to your list by entering them in the text box and clicking the button. Mark an item as complete by selecting the check box. Once an item is marked as complete, it can be deleted by clicking the delete button.
 
-Don't have time to finish all of your list now? Your list will reload when you revist the app or reload the page!
+Don't have time to finish all of your list now? Your list will reload when you revist the app or reload the page using local storage!
 
 Access the app [here](https://danielmurphy1.github.io/ToDoApp/)
 
@@ -20,6 +20,8 @@ I created this app to learn about DOM manipulation and how to use local storage 
 
 It took me approxiamtely 17 hours to learn the skills needed for this app, write the code, and to debug. I learned a lot about manipulating and traversing the DOM and how to set and retrieve data to and from local browser storage. It was fun to solve the problems that I ran into while developing this app.
 
+Update: I have revistited this application approximately 3 months after first writing it for editing and refactoring. The structure of the application remains the same; however, much of the code has been refactored to make the code more readable and to put code that was reused into reusable functions. In essence, I have taken what I have learned in the last few months and applied it to this earlier project. Enjoy comparing the old code to the new!
+
 ### Author
 
-- Dan Murphy
+- Dan Murphy. Software Developer
--- script.js
+++ script.js
@@ -1,124 +1,88 @@
 const addBtn = document.getElementById("add-btn");
-const todoTextInput = document.getElementById("todo-text-input");
-const ul = document.getElementById("list");
-const error = document.getElementById("error");
-let toDoList = [];
+addBtn.addEventListener("click",  function(event){
+    const todoTextInput = document.getElementById("todo-text-input");
+    const error = document.getElementById("error");
 
+    event.preventDefault();
 
-addBtn.addEventListener("click",  function(){
     if (todoTextInput.value === ""){
-        event.preventDefault();
         error.classList.remove("hidden");
-    }else {
-        error.classList.add("hidden");
-        console.log(todoTextInput.value);
-        event.preventDefault();
-        let li = document.createElement("li");
-        let listItemID = Math.random();
-        li.setAttribute("id", listItemID);
-        let liText = document.createTextNode(todoTextInput.value);
-
-        let checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-            
-        
-        let deleteButton = document.createElement("button");
-            deleteButton.classList.add("hidden");
-            deleteButton.textContent = "Delete";
-      
-       
-        
-        li.appendChild(checkbox);
-        li.appendChild(liText);
-        li.appendChild(deleteButton);
-        ul.appendChild(li);
-        
-        
-        const toDoItem = {
-            id: listItemID, 
-            text: todoTextInput.value,
-        };
-        todoTextInput.value = "";
-        toDoList.push(toDoItem);
-          
-
-
-         
-         window.localStorage.setItem('user', JSON.stringify(toDoList));
-        
-
-     
-
-        deleteButton.addEventListener("click", function(){
-            li.remove();
-            const index = toDoList.indexOf(toDoItem);
-            if (index > -1){
-                toDoList.splice(index, 1);
-            }
-            
-            window.localStorage.setItem('user', JSON.stringify(toDoList));
-           
-
-        });
-       
-
-        checkbox.addEventListener("click",function(){
-            deleteButton.classList.toggle("hidden");
-            li.classList.toggle("complete");
-            
-        });
-        
-        
+        return;
     }
 
-    
+    error.classList.add("hidden");
+
+    const toDoItem = {
+        id: Math.random(), 
+        text: todoTextInput.value,
+    };
+
+    renderToDoItem(toDoItem);
+
+    todoTextInput.value = "";
+
+    const toDoList = JSON.parse(window.localStorage.getItem('user')) || [];
+    toDoList.push(toDoItem);
+
+    window.localStorage.setItem('user', JSON.stringify(toDoList));   
 });
 
-window.onload = function(){
-    toDoList = JSON.parse(window.localStorage.getItem('user')) || [];
-    for (let i = 0; i < toDoList.length; i++){
-        console.log(toDoList[i]);
-        
-        
+function createLiElement(toDoItem){
+    let li = document.createElement("li");
 
-        let li = document.createElement("li");
-        let listItemID = toDoList[i]["id"];
-        li.setAttribute("id", listItemID);
-        let liText = document.createTextNode(toDoList[i]["text"]);
+    li.setAttribute("id", toDoItem.id);
 
-
-        let checkbox = document.createElement("input");
-            checkbox.type = "checkbox";
-           
-
-        let deleteButton = document.createElement("button");
-            deleteButton.classList.add("hidden");
-            deleteButton.textContent = "Delete";
-
-        
-        li.appendChild(checkbox);
-        li.appendChild(liText);
-        li.appendChild(deleteButton);
-        ul.appendChild(li);
-
-        
-        deleteButton.addEventListener("click", function(){
-            li.remove();
-            const index = toDoList.findIndex(item => item.id === listItemID);
-            if (index > -1){
-                toDoList.splice(index, 1);
-            }
-            
-            window.localStorage.setItem('user', JSON.stringify(toDoList));
-           
-
-        });    
-
-        checkbox.addEventListener("click",function(){
-            deleteButton.classList.toggle("hidden");
-            li.classList.toggle("complete");
-        });
-    }
+    return li;
 }
 
+function createCheckbox(li, deleteButton) {
+    let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
 
+    checkbox.addEventListener("click",function(){
+        deleteButton.classList.toggle("hidden");
+        li.classList.toggle("complete");        
+    }); 
+
+        return checkbox;
+}
+
+function createDeleteButton(li, toDoItem){
+    let deleteButton = document.createElement("button");
+    deleteButton.classList.add("hidden");
+    deleteButton.textContent = "Delete";
+
+    deleteButton.addEventListener("click", function(){
+        li.remove();
+        const toDoList = JSON.parse(window.localStorage.getItem('user')) || [];
+        const index = toDoList.findIndex(item => item.id === toDoItem.id);
+        if (index > -1){
+            toDoList.splice(index, 1);
+        }
+        
+        window.localStorage.setItem('user', JSON.stringify(toDoList));
+    }); 
+
+    return deleteButton;
+}
+
+function renderToDoItem(toDoItem){
+    const li = createLiElement(toDoItem);
+
+        let deleteButton = createDeleteButton(li, toDoItem);
+
+        let checkbox = createCheckbox(li, deleteButton);
+
+        li.appendChild(checkbox);
+        li.appendChild(document.createTextNode(toDoItem["text"]));
+        li.appendChild(deleteButton);
+        const ul = document.getElementById("list");
+        ul.appendChild(li);
+}
+
+window.onload = function(){
+    const toDoList = JSON.parse(window.localStorage.getItem('user')) || [];
+    for (let i = 0; i < toDoList.length; i++){
+        renderToDoItem(toDoList[i]);
+    }
+}
